8923f2c9cea449ef3fe119b764233d91ffc4f127
cecile hirschauer <cecile.h.75@gmail.com>
2023-09-28 10:21:35 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import os;
+import openai
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, Response
+
+load_dotenv()
+
+app = Flask(__name__)
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/prompt', methods=["POST"])
+def prompt():
+    messages = request.json['messages']
+    conversation = build_conversation_dict(messages)
+
+    return Response(event_stream(conversation), mimetype='text/event-stream')
+
+
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {"role": "user" if i % 2 == 0 else "assistant", "content": message}
+        for i, message in enumerate(messages)
+    ]
+
+
+def event_stream(conversation: list[dict]) -> str:
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+        stream=True
+    )
+
+    for text_line in response:
+        text = text_line.choices[0].delta.get('content', '')
+        if text:
+            yield text
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
+    new_conversation = build_conversation_dict(messages=["Bonjour, comment ça va ?", "Ca va bien et toi ?"])
+    for line in event_stream(new_conversation):
+        print(line)
+
---
+++ static/dist/js/script.js
@@ -0,0 +1,48 @@
+function cloneAnswerBlock() {
+    const output = document.querySelector('#gpt-output');
+    const template = document.querySelector('#chat-template');
+    const clone = template.cloneNode(true);
+    clone.id = "";
+    output.appendChild(clone);
+    clone.classList.remove("hidden");
+    return clone.querySelector(".message");
+};
+
+function addToLog(message) {
+    const answerBlock = cloneAnswerBlock();
+    answerBlock.innerText = message;
+    return answerBlock;
+}
+
+function getChatHistory() {
+    const messageBlocks = document.querySelectorAll('.message:not(#chat-template .message)');
+    return Array.from(messageBlocks).map((block) => block.innerText);
+}
+
+async function fetchPromptResponse() {
+    await fetch('/prompt', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'messages': getChatHistory()
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#prompt-form');
+    const spinnerIcon = document.querySelector('#spinner-icon');
+    const sendIcon = document.querySelector('#send-icon');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        spinnerIcon.classList.remove('hidden');
+        sendIcon.classList.add('hidden');
+
+        const prompt = form.elements.prompt.value;
+        addToLog(prompt);
+    })
+});
+
